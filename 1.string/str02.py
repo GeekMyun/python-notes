35117881fb2019a33c2e7245ee707d7f77e42128
GeekMyun <3288610969@qq.com>
2026-08-12 19:42:48 +0800

    update

---
+++ 1.string/str02.py
@@ -0,0 +1,19 @@
+"""
+1.声明字符串
+- '单引号'和"双引号"
+- '''三单引号'''和三单双引号：保留换行和缩进，通常用于SQL和文档注释
+"""
+str0 = 'hello word'
+str1 = "hello word"
+str2 = '''
+hello
+word
+'''
+str3 = """
+select * from user where PL='python'
+"""
+print(str0,str1,str2,str3)
+
+"""
+
+"""
